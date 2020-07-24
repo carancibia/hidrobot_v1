6a973201d5cc1bdf2f136fc9544536046c920cf4
carancibia <51372427+carancibia@users.noreply.github.com>
2020-07-24 01:44:27 -0300

    Update hidrobot.ino

--- hidrobot.ino
+++ hidrobot.ino
@@ -1,35 +1,69 @@
 /*
+*******************************************************************
   Version     : 1.0 
   Autor       : Claudio Arancibia
-  Empresa     : IOTechnology
+  Empresa     : MySmart Solutions Ltda.
   Fecha       : 02-06-2019
   Descripcion : Version basica de sistema de Hidroponia/Aeroponia
                 Cargar sobre Arduino Uno con Rele Shield 
+*******************************************************************
+*******************************************************************
+*                     INICIO DEL PROGRAMA
+*******************************************************************
+*******************************************************************
+* Inicializamos las Variables a utilizar
+*******************************************************************
 */
 
+// Almacenamos en la variable rele el numero del pin a utilizar
 
-// Declaramos el canal por el que vamos a enviar pulsos de 5volt
-// Este sera el pin 13 del Arduino Uno
+// Utilizamos el Pin 13 de Arduino porque ya contien una reistencia 
+// interna, con eso nos ahorramos incluir otra en el circuito.
 
 int rele = 13;
- 
-// Inicializaremos las variables del sistema en el metodo Setup
+
+/*
+*******************************************************************
+* Configuramos - Iniciamos las Variables
+*******************************************************************
+*/
 
 void setup() 
 {
+  //Para el puerto serial, iniciamos en 9600 baudios.
   Serial.begin(9600);
-  // Declaramos que el Pin 13 del arduino sera un canal de salida.
+  
+  //Declaramos que el Pin 13 del arduino sera un canal de salida.
+  //Por allí vamos a mandar un pulso de 5 volt cada vez que necesitemos
+  
   pinMode(rele, OUTPUT);
 }
- 
-// La funcion loop se ejecuta constantemente una y otra vez
+
+/*
+*******************************************************************
+* Ejecutamos el codigo de Loop que es lo que se estara ejecutando una y otra y otra...
+* vez
+*******************************************************************
+*/
 
 void loop() 
 {
-  digitalWrite(rele, HIGH);   // Mandamos a la salida del pin 13, 5 volt con lo que encendemos la bomba de agua
-  Serial.println("Encendido...");
-  delay(30000);              // Regamos por 30 Segundos
-  digitalWrite(rele, LOW);    // Apagamos el rele para detener la bomba de agua
-  Serial.println("Apagado...");
-  delay(300000);             // descanzamos 5 minutos y luego volvemos a regar. 
+  // Mandamos a la salida del pin 13, 5 volt con lo que encendemos la bomba de agua
+  digitalWrite(rele, HIGH);   
+  Serial.println("Iniciando el riego presurizado.");
+  // Delay entiende el parametro en Milisegundos entonces 1000 equivale a 1 segundo
+  delay(5000);              
+  // Regamos por 5 Segundos, para el ejemplo
+
+  //Ahora dejamos de mandar el pulso de 5 volt y se corta el suministro.
+  digitalWrite(rele, LOW);    
+  // Apagamos el rele para detener la bomba de agua
+  Serial.println("Termino el proceso de riego presurizado.");
+  delay(10000);             
+  // descanzamos 10 segundos y luego volvemos a regar. 
 }
+/*
+*******************************************************************
+*                     FIN DEL PROGRAMA
+* *****************************************************************
+*/
